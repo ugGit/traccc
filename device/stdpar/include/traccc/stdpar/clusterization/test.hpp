ac034f3613ce2924e0532a321a0a62b4d0fde27d
Uchendu Nwachukwu <nwachuch@students.zhaw.ch>
2022-03-31 16:37:44 -0700

    finalize build setup with example to implement stdpar classes

---
+++ device/stdpar/include/traccc/stdpar/clusterization/test.hpp
@@ -0,0 +1,16 @@
+/** TRACCC library, part of the ACTS project (R&D line)
+ *
+ * (c) 2022 LBNL for the benefit of the ACTS project
+ *
+ * Mozilla Public License Version 2.0
+ */
+
+#pragma once
+
+namespace traccc {
+namespace stdpar {
+
+void execute();
+
+}  // namespace stdpar
+}  // namespace traccc
