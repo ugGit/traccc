bce5090208f947375b10ef7acfa07c2e31391e7d
Uchendu Nwachukwu <nwachuch@students.zhaw.ch>
2022-04-03 10:33:42 -0700

    run local to global transofmration parallel (not yet par_unseq)

--- device/stdpar/include/traccc/stdpar/clusterization/test.hpp
+++ device/stdpar/include/traccc/stdpar/clusterization/test.hpp
@@ -7,9 +7,16 @@
 
 #pragma once
 
+#include "traccc/edm/measurement.hpp"
+#include "traccc/edm/spacepoint.hpp"
+
 namespace traccc {
 namespace stdpar {
 
+void local_to_global(const cell_module& module, 
+                     measurement *measurements_array, 
+                     spacepoint *spacepoints_array, 
+                     const int number_of_measurements);
 void execute();
 
 }  // namespace stdpar
