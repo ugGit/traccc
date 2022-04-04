4a1b8db8f87942e7c636fdb6b4d045e7afdd25ca
Uchendu Nwachukwu <nwachuch@students.zhaw.ch>
2022-04-04 15:19:31 -0700

    working version par unseq inlined

--- device/stdpar/src/clusterization/test.cpp
+++ device/stdpar/src/clusterization/test.cpp
@@ -14,39 +14,17 @@
 #include <stdio.h>
 
 
-#include "traccc/stdpar/clusterization/test.hpp"
-#include "traccc/stdpar/utils/CountingIterator.h"
-
 namespace traccc {
 namespace stdpar {
 
 using std::cout;
 
-void local_to_global(const cell_module& module, 
-                     measurement *measurements_array, 
-                     spacepoint *spacepoints_array, 
-                     const int number_of_measurements){
-    std::for_each_n(std::execution::par, counting_iterator(0), number_of_measurements, 
-        [=](unsigned int i){
-            const auto m = measurements_array[i];
-            
-            point3 local_3d = {m.local[0], m.local[1], 0.};
-            
-            point3 global = module.placement.point_to_global(local_3d);
-            variance3 variance = {0, 0, 0};
-            spacepoint s({global, variance, m});
-            
-            // @todo add variance estimation
-            // spacepoints_array[i] = s;
-            spacepoints_array[i] = {global, variance, m};
-        }
-    ); 
-}
-
 void execute(){
   const int DSIZE = 32; // 2*32*1048576;
+  /*
   cout << "Start Vector Add Program\n";
   cout << "-----------\n";
+  */
 
   // Try to access elements first with vectors,
   // and in a second round by random access in arrays
@@ -64,7 +42,7 @@ void execute(){
   const auto t1 = std::chrono::high_resolution_clock::now();
 
   // execute 
-  std::transform(std::execution::par, a.begin(), a.end(), b.begin(), c.begin(), [](float x, float y) -> float {return x+y;});
+  std::transform(std::execution::par_unseq, a.begin(), a.end(), b.begin(), c.begin(), [](float x, float y) -> float {return x+y;});
   
   // stop crono
   const auto t2 = std::chrono::high_resolution_clock::now();
