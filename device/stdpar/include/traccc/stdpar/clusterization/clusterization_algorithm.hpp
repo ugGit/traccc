2ad304cc062079b43b0e38a32c90668f70880d69
Uchendu Nwachukwu <nwachuch@students.zhaw.ch>
2022-05-17 11:51:01 -0700

    properly format file header

--- device/stdpar/include/traccc/stdpar/clusterization/clusterization_algorithm.hpp
+++ device/stdpar/include/traccc/stdpar/clusterization/clusterization_algorithm.hpp
@@ -1,4 +1,5 @@
-/** TRACCC library, part of the ACTS project (R&D line)
+/*
+ * TRACCC library, part of the ACTS project (R&D line)
  *
  * (c) 2021-2022 CERN for the benefit of the ACTS project
  *
