598a3401442dab47f2a9d4a3c14f7ec0f422d821
Uchendu Nwachukwu <nwachuch@students.zhaw.ch>
2022-05-31 17:02:08 -0700

    backport measurement creation (excluding helper)

--- device/stdpar/include/traccc/stdpar/clusterization/measurement_creation.hpp
+++ device/stdpar/include/traccc/stdpar/clusterization/measurement_creation.hpp
@@ -7,7 +7,7 @@
 
 #pragma once
 
-#include "traccc/stdpar/clusterization/measurement_creation_helper.hpp"
+// #include "traccc/stdpar/clusterization/measurement_creation_helper.hpp"
 #include "traccc/definitions/primitives.hpp"
 #include "traccc/edm/cell.hpp"
 #include "traccc/edm/cluster.hpp"
@@ -15,6 +15,48 @@
 #include "traccc/utils/algorithm.hpp"
 
 namespace traccc::stdpar {
+
+
+/// Function used for retrieving the cell signal based on the module id
+inline scalar signal_cell_modelling(scalar signal_in,
+                                    const cluster_id& /*cl_id*/) {
+    return signal_in;
+}
+
+/// Function for pixel segmentation
+inline vector2 position_from_cell(cell c, cluster_id cl_id) {
+    // Retrieve the specific values based on module idx
+    return {cl_id.pixel.min_center_x + c.channel0 * cl_id.pixel.pitch_x,
+            cl_id.pixel.min_center_y + c.channel1 * cl_id.pixel.pitch_y};
+}
+
+inline vector2 get_pitch(cluster_id cl_id) {
+    // return the values based on the module idx
+    return {cl_id.pixel.pitch_x, cl_id.pixel.pitch_y};
+}
+
+/// Function used for calculating the properties of the cluster inside
+/// measurement creation
+inline void calc_cluster_properties(
+    cell* cluster, unsigned int cluster_size, const cluster_id& cl_id, point2& mean,
+    point2& var, scalar& totalWeight) {
+    for(int j=0; j < cluster_size; j++){
+        const auto& cell = cluster[j];
+        scalar weight = signal_cell_modelling(cell.activation, cl_id);
+        if (weight > cl_id.threshold) {
+            totalWeight += cell.activation;
+            const point2 cell_position = position_from_cell(cell, cl_id);
+            const point2 prev = mean;
+            const point2 diff = cell_position - prev;
+
+            mean = prev + (weight / totalWeight) * diff;
+            for (std::size_t i = 0; i < 2; ++i) {
+                var[i] =
+                    var[i] + weight * (diff[i]) * (cell_position[i] - mean[i]);
+            }
+        }
+    }
+}
 
 /// Connected component labeling.
 struct measurement_creation
@@ -55,31 +97,17 @@ struct measurement_creation
     ///
     /// @return a measurement collection - usually same size or sometime
     /// slightly smaller than the input
-    void operator()(const host_cluster_container &clusters,
+    void operator()(const cluster_element *clusters,
                     const cell_module &module,
-                    output_type &measurements) const {
+                    const unsigned int num_clusters,
+                    measurement*& measurements) {
 
         // Run the algorithm
         auto pitch = module.pixel.get_pitch();
 
+        measurements = new measurement[num_clusters];  
 
-        auto clusters_items = clusters.get_items();
-        int number_of_clusters = clusters_items.size();
-        measurements.reserve(number_of_clusters);
-        vecmem::vector<traccc::cell> *clusters_array = new vecmem::vector<traccc::cell>[number_of_clusters]; 
-
-        // populate the array by copying the values
-        for (int i = 0; i < number_of_clusters; i++){
-            clusters_array[i] = clusters_items.at(i);
-        }
-
-        /* This causes an error on push back of measurement at the very end of the function.
-        // TODO: add closing paranthese ')' at end of function if uncommented.
-        std::for_each_n(std::execution::par_unseq, counting_iterator(0), number_of_clusters, 
-            [=](unsigned int j){
-                const auto &cluster = clusters_array[j];
-                */
-        for (const auto &cluster : clusters.get_items()) {
+        for(int i=0; i < num_clusters; i++){
             scalar totalWeight = 0.;
 
             // To calculate the mean and variance with high numerical stability
@@ -94,15 +122,15 @@ struct measurement_creation
             point2 mean = {0., 0.}, var = {0., 0.};
 
             // Should not happen
-            if (cluster.empty()) {
+            if (clusters[i].items_size == 0) {
                 continue;
             }
 
             // Get the cluster id for this module
-            const auto &cl_id = clusters.at(0).header;
+            const auto &cl_id = clusters[i].header;
 
-            // Calculate the cluster properties
-            traccc::stdpar::calc_cluster_properties<vecmem::vector, cell>(cluster, cl_id, mean,
+            // Calculate the cluster properties        
+            calc_cluster_properties(clusters[i].items, clusters[i].items_size, cl_id, mean,
                                                           var, totalWeight);
 
             if (totalWeight > 0.) {
@@ -114,9 +142,9 @@ struct measurement_creation
                 m.variance[1] = var[1] / totalWeight;
                 // plus pitch^2 / 12
                 m.variance = m.variance + point2{pitch[0] * pitch[0] / 12,
-                                                 pitch[1] * pitch[1] / 12};
+                                                  pitch[1] * pitch[1] / 12};
                 // @todo add variance estimation
-                measurements.push_back(std::move(m));
+                measurements[i] = std::move(m);
             }
         }
     }
