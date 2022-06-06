7316f24eabdac4c38f6822890671f22c2c79da51
Uchendu Nwachukwu <nwachuch@students.zhaw.ch>
2022-06-06 09:56:27 -0700

    update to latest test struct that should supposedly work

--- tests/stdpar/test_cca_fastsv.cpp
+++ tests/stdpar/test_cca_fastsv.cpp
@@ -13,21 +13,15 @@
 #include "traccc/stdpar/clusterization/component_connection_ssv.hpp"
 
 namespace {
-traccc::stdpar::component_connection_ssv cc;
+
+traccc::stdpar::component_connection_ssv ca;
 
 cca_function_t f = [](const traccc::cell_container_types::host &data) {
     std::map<traccc::geometry_id, vecmem::vector<traccc::measurement>> result;
 
-    traccc::measurement_container_types::host mss = cc(data);
-
-    for (std::size_t i = 0; i < mss.size(); ++i) {
-        vecmem::vector<traccc::measurement> msv;
-
-        for (std::size_t j = 0; j < mss.at(i).items.size(); ++j) {
-            msv.push_back(mss.at(i).items.at(j));
-        }
-
-        result.emplace(mss.at(i).header.module, std::move(msv));
+    auto measurements = ca(data);
+    for (std::size_t i = 0; i < measurements.size(); i++) {
+        result[measurements.at(i).header.module] = measurements.at(i).items;
     }
 
     return result;
