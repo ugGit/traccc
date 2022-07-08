815143d9601ac8052083aa7a740d8894efe438cf
Uchendu Nwachukwu <nwachuch@students.zhaw.ch>
2022-07-08 11:28:21 -0700

    backport custom edm

--- device/stdpar/include/traccc/stdpar/clusterization/detail/cluster_element.hpp
+++ device/stdpar/include/traccc/stdpar/clusterization/detail/cluster_element.hpp
@@ -6,8 +6,17 @@
 // System include(s).
 #include <cstddef>
 
+struct cluster_id {
+    event_id event = 0;
+    std::size_t module_idx = 0;
+    geometry_id module = 0;
+    scalar threshold = 0.;
+    transform3 placement = transform3{};
+    pixel_data pixel;
+};
+
 struct cluster_element{
-  std::size_t header;
+  cluster_id header;
   traccc::cell* items;
   unsigned int items_size;
 };
