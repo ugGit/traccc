6121fb6e17107add257515c5f81036fa13ac2264
Uchendu Nwachukwu <nwachuch@students.zhaw.ch>
2022-07-12 17:52:38 -0700

    fix references to namespaced types

--- device/stdpar/include/traccc/stdpar/clusterization/detail/cluster_element.hpp
+++ device/stdpar/include/traccc/stdpar/clusterization/detail/cluster_element.hpp
@@ -2,17 +2,18 @@
 #pragma once
 
 #include "traccc/edm/cell.hpp"
+#include "traccc/definitions/primitives.hpp"
 
 // System include(s).
 #include <cstddef>
 
 struct cluster_id {
-    event_id event = 0;
+    traccc::event_id event = 0;
     std::size_t module_idx = 0;
-    geometry_id module = 0;
-    scalar threshold = 0.;
-    transform3 placement = transform3{};
-    pixel_data pixel;
+    traccc::geometry_id module = 0;
+    traccc::scalar threshold = 0.;
+    traccc::transform3 placement = traccc::transform3{};
+    traccc::pixel_data pixel;
 };
 
 struct cluster_element{
